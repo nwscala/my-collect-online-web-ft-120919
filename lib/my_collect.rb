63f2912945ee2fa731bb090829ce097fd9f49907
nwscala <nwscala@gmail.com>
2019-12-11 18:02:23 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
+def my_collect(array)
+    index = 0
+    collection = []
+    while index < array.length
+        collection << yield(array[index])
+        index += 1
+    end
+    collection
+end
 
